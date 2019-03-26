9b920493f1ccdb7e788080496be1aff77d1eed7f
ZexingGao <zexing@bu.edu>
2019-03-26 17:18:55 -0400

    css

--- js/login.js
+++ js/login.js
@@ -1,24 +1,4 @@
-function hasClass(elem, cls) {
-    cls = cls || '';
-    if (cls.replace(/\s/g, '').length == 0) return false; //If there is no func in cls, then return false
-    return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
-}
 
-function addClass(ele, cls) {
-    if (!hasClass(ele, cls)) {
-        ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
-    }
-}
-
-function removeClass(ele, cls) {
-    if (hasClass(ele, cls)) {
-        var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
-        while (newClass.indexOf(' ' + cls + ' ') >= 0) {
-            newClass = newClass.replace(' ' + cls + ' ', ' ');
-        }
-        ele.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
 
 
 //Sign In button Function
@@ -65,3 +45,28 @@ function initApp() {
 window.onload = function() {
     initApp();
 };
+
+
+
+/*function hasClass(elem, cls) {
+    cls = cls || '';
+    if (cls.replace(/\s/g, '').length == 0) return false; //If there is no func in cls, then return false
+    return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
+}
+
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) {
+        ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
+    }
+}
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
+        while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+            newClass = newClass.replace(' ' + cls + ' ', ' ');
+        }
+        ele.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+*/
